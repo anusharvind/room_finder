023399d5f009615966a9d2ea48b33235ecb883e4
priya <prissheiya@gmail.com>
2016-05-07 13:58:49 +0530

    Home Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,10 @@ module ApplicationHelper
 			image_tag("room-finder-logo.png", :alt => "App", :class => "round", :width => 200, :height => 100)
 		end
 
+		def show_image(image)
+			image_tag(image, :alt => "App", :class => "demo w3-border w3-hover-shadow", :style => "width:30%")
+		end
+
+
 
 end
